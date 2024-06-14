f16256271bd14862863c9255e0e530bc4f713253
jaydeep8007 <parmarjaydeep018@gmail.com>
2024-06-14 23:20:39 +0530

    professional backend setup completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,9 +11,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    });
+    // res.status(200).json({
+    //     message: "ok"
+    // });
 });
 
 export { registerUser };
