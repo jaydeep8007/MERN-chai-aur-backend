28b8b9ed429d0bba32c21271933be20e95c033df
jaydeep8007 <parmarjaydeep018@gmail.com>
2024-06-15 15:33:29 +0530

    add ApiError and ApiResponce file in utils

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,9 +11,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    // res.status(200).json({
-    //     message: "ok"
-    // });
+    //get user details from frontend
+    //validation - not empty
+    //if user already exist
+    //check for images - check for avatar 
+    //upload them to cloudinary - avatar
+    //create user object  , create entry in db
+    //remove refresh token from responce
+    //check for user creation 
+    //return responce 
+
+    const {fullName , email , username , password} = req.body 
+    console.log('email : ', email)
 });
 
 export { registerUser };
